c75b68bc503c8d300949e2ac50e3129b56e2863d
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-07-18 06:50:25 +0530

    added controller for signup and about pages

--- smartcontactmanager/src/main/java/com/smart/controllers/MainController.java
+++ smartcontactmanager/src/main/java/com/smart/controllers/MainController.java
@@ -7,9 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-	@GetMapping("/home")
+	@GetMapping("/")
 	public String homeController(Model model) {
 		model.addAttribute("title","Home - Smart Contact Manager");
 		return "home";
+	}
+	
+	@GetMapping("/about")
+	public String aboutController(Model model) {
+		model.addAttribute("title","About - Smart Contact Manager");
+		return "about";
+	}
+	
+	@GetMapping("/signup")
+	public String signupController(Model model) {
+		model.addAttribute("title","Register - Smart Contact Manager");
+		return "signup";
 	}
 }
